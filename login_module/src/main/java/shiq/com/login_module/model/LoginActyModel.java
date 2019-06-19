8810f68e035050b8d6b52b74f6ed8c1114964fba
史强 <shiqiang@julong.cc>
2019-06-19 11:19:33 +0800

    删除重新导入数据

--- login_module/src/main/java/shiq/com/login_module/model/LoginActyModel.java
+++
@@ -1,9 +0,0 @@
-package shiq.com.login_module.model;
-
-import shiq.com.login_module.contract.LoginActyContract;
-
-/**
- * created by shi on 2019/6/12/012
- */
-public class LoginActyModel implements LoginActyContract.Model {
-}
