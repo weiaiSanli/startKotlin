a2bb9bab64cdb51e94520ecbb7053727540e0802
史强 <shiqiang@julong.cc>
2019-07-05 17:58:50 +0800

    新增协程的设置

--- login_module/src/main/java/shiq/com/login_module/activity/SplashActivity.kt
+++ login_module/src/main/java/shiq/com/login_module/activity/SplashActivity.kt
@@ -7,10 +7,7 @@ import android.widget.Toast
 import com.alibaba.android.arouter.launcher.ARouter
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import shiq.com.common.base.BaseActivity
 import shiq.com.common.utils.*
 import shiq.com.login_module.R
@@ -22,7 +19,7 @@ import kotlin.coroutines.CoroutineContext
 /**
  * created by shi on 2019/6/13/013
  */
-class SplashActivity : BaseActivity(){
+class SplashActivity : BaseActivity() {
 
     //进入页面的时间
     private var initTime: Long = 0
@@ -40,19 +37,29 @@ class SplashActivity : BaseActivity(){
     }
 
     override fun initData() {
-
-        GlobalScope.launch {
+        "我是开始".log()
+        val job = GlobalScope.launch(Dispatchers.Main) {
+            //默认为DIspatchers.Default
 
             val token = requestToken()
-            val post = createPost(token , "123")
-            processPost(post)
+            val post = createPost(token, "123")
+            val wait = async(Dispatchers.IO) { //设置在IO线程中计算数据
+                delay(3000) //只能在协程中使用,挂起协程,但不会阻塞线程
+                processPost(post)
+            }
+            wait.await().log() //await得到async返回的结果,运行线程跟launch协程一致
+            "我是结果".log()
+            //需要设置协程运行的线程为UI线程,不设置使用默认线程池会报子线程不能更新UI
+            toast("我是结果")
         }
+        "我是结束".log()
 
-
+        job.cancel() //取消协程任务
 
 //        enitAcitivity()
 
     }
+
     //被suspend用作修饰会被暂停的函数，被标记为 suspend 的函数只能运行在协程或者其他 suspend 函数当中。
     suspend fun requestToken(): Token {
         return Token("shiq")
@@ -61,21 +68,23 @@ class SplashActivity : BaseActivity(){
     /**
      * 设置suspend函数
      */
-    suspend fun createPost(token:Token , pwd:String): Post{
-        if (token.token == "shiq" && pwd == "123"){
+    suspend fun createPost(token: Token, pwd: String): Post {
+        if (token.token == "shiq" && pwd == "123") {
             return Post("success")
         }
         return Post("error")
     }
 
-    fun processPost(post: Post) {
+    suspend fun processPost(post: Post): String {
 
-        if (post.success == "success"){
-            toast("登录成功")
-        }else{
-            toast("登录失败")
+//        delay(3000)
+        return if (post.success == "success") {
+//            ("登录成功").log()
+            "登录成功"
+        } else {
+//            ("登录失败").log()
+            "登录失败"
         }
-
     }
 
     /**
